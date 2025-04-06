772c803106ea77692830f5255ecf74b11e0080ff
Ananya Narang <ananyanarang1411@gmail.com>
2025-04-06 22:54:12 +0530

    Update Nav.jsx

--- src/Pages/Landing Page/Nav.jsx
+++ src/Pages/Landing Page/Nav.jsx
@@ -6,12 +6,12 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 function Nav() {
   return (
     <>
-      <div className="navBar">
-        <div className="navbar">
-          <div className="logoCon">
+      <div className="navbar">
+        <div className="navbar-container">
+          <div className="logo-container">
             <h1 className="logo">Movie</h1>
           </div>
-          <div className="menuCon">
+          <div className="menu-container">
             <ul className="menu-list">
               <li className="menu-list-item active">Home</li>
               <li className="menu-list-item">Movies</li>
@@ -20,22 +20,22 @@ function Nav() {
               <li className="menu-list-item">Trends</li>
             </ul>
           </div>
-          <div className="profileCon">
-            <img className="profilePic" src="https://static-00.iconduck.com/assets.00/user-female-icon-1488x2048-6bjwjktq.png" alt="profile" />
-            <div className="ProfileText">
+          <div className="profile-container">
+            <img className="profile-picture" src="https://static-00.iconduck.com/assets.00/user-female-icon-1488x2048-6bjwjktq.png" alt="profile" />
+            <div className="profile-text-container">
               <span className="profile-text">Profile</span>
             </div>
           </div>
         </div>
       </div>
       <div className="sidebar">
-        <i className="left-icon fas fa-search"></i>
-        <i className="left-icon fas fa-home"></i>
-        <i className="left-icon fas fa-users"></i>
-        <i className="left-icon fas fa-bookmark"></i>
-        <i className="left-icon fas fa-tv"></i>
-        <i className="left-icon fas fa-hourglass-start"></i>
-        <i className="left-icon fas fa-shopping-cart"></i>
+        <i className="left-menu-icon fas fa-search"></i>
+        <i className="left-menu-icon fas fa-home"></i>
+        <i className="left-menu-icon fas fa-users"></i>
+        <i className="left-menu-icon fas fa-bookmark"></i>
+        <i className="left-menu-icon fas fa-tv"></i>
+        <i className="left-menu-icon fas fa-hourglass-start"></i>
+        <i className="left-menu-icon fas fa-shopping-cart"></i>
       </div>
     </>
   );
